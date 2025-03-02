79341886c9ba3a6e60a3898bc2d53a0c450ee349
Murat Kılıç <158284118+muratkilic04@users.noreply.github.com>
2025-03-02 17:19:32 +0300

    Add files via upload

---
+++ Hipotenüs Bulma/ornekler/src/ornekler/orneklerr.java
@@ -0,0 +1,30 @@
+package ornekler;
+
+import java.util.Scanner;
+
+public class orneklerr {
+
+	public static void main(String[] args) {
+		
+		 Scanner input=new Scanner(System.in);
+		 double a,b,c;
+		 System.out.println("a:");
+		 a=input.nextDouble();
+		 System.out.println("b:");
+		 b=input.nextDouble();
+		 
+		 c=Math.sqrt(a*a+b*b);
+		 
+		 System.out.println("Hipotenüs:"+c);
+		 
+		
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+
+}
